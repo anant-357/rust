de6013bd41efc9f7cf6c2ede06c99af95d41764f
anant <95141712+anant-357@users.noreply.github.com>
2022-07-31 14:46:11 +0530

    minigrep comlete?v1.0

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,15 +1,11 @@
 use std::fs;
 use std::error::Error;
-<<<<<<< HEAD
 use std::env;
-=======
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
 
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-<<<<<<< HEAD
     let results = if config.case_sensitive {
         search(&config.query, &contents)
     }else{
@@ -17,9 +13,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     };
     
     for line in results{
-=======
-    for line in search(&config.query, &contents){
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
         println!("{}",line);
     }
     
@@ -29,10 +22,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
-<<<<<<< HEAD
     pub case_sensitive: bool,
-=======
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
 }
 
 impl Config {
@@ -44,13 +34,9 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-<<<<<<< HEAD
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         return Ok(Config { query, filename, case_sensitive });
-=======
-        return Ok(Config { query, filename });
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
     }
 }
 
@@ -65,7 +51,6 @@ pub fn search<'a>(query: &str, contents: &'a str)-> Vec<&'a str>{
     results
 }
 
-<<<<<<< HEAD
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str)-> Vec<&'a str>{
     let mut results = Vec::new();
 
@@ -77,22 +62,15 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str)-> Vec<&'a str
     results
 }
 
-=======
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
 #[cfg(test)]
 mod tests {
     use super ::*;
     #[test]
-<<<<<<< HEAD
     fn case_sensitive(){
-=======
-    fn one_result(){
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
         let query = "duct";
         let contents = "\
 Rust:
 safe,fast,productive.
-<<<<<<< HEAD
 Pick three.
 Duct tape.";
         assert_eq!(vec!["safe,fast,productive."], search(query, contents));
@@ -107,9 +85,5 @@ Pick three
 Trust me.
         ";
         assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query,contents));
-=======
-Pick three.";
-        assert_eq!(vec!["safe,fast,productive."], search(query, contents));
->>>>>>> 075306f5301d2a48bfa86f7d451d009c52ab293b
     }
 }
