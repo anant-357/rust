9b1c2a161023e538fd7ac0e2bc2b806f7806bc51
anant <95141712+anant-357@users.noreply.github.com>
2022-08-01 15:42:02 +0530

    handling non-number error and finishing up

--- Learning/guessing_game/src/main.rs
+++ Learning/guessing_game/src/main.rs
@@ -4,26 +4,36 @@ use rand:: Rng;
 fn main(){
 
     let truth = rand::thread_rng().gen_range(1..=100);
-    println!("{truth}");
 
     loop {
         println!("Enter your guess:");
-
         let mut guess = String :: new();
         io:: stdin().read_line(&mut guess).expect("Failed to read guess");
+
         
-        let guess : i32 = guess.trim().parse().expect("Please type a number!");
+        let guess : i32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            },
+        };
+
 
         println!("You guessed {}", guess);
 
+        
         if guess == truth {
             println!("You guessed correct");
             break;
-
         }
         else{
-            println!("Oho Maa chuda");
-            println!("Abe kaha ja raha hai. Chal ek aur baar try kar le.");
+            if guess > truth {
+                println!("Too large");
+            }
+            else{
+                println!("Too small");
+            }
         }
     }
     println!("You win!");
