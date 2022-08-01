408d82f80fd3dadd4df031ec3595485035955145
anant <95141712+anant-357@users.noreply.github.com>
2022-08-01 15:01:53 +0530

    added loop and end

--- Learning/guessing_game/src/main.rs
+++ Learning/guessing_game/src/main.rs
@@ -2,24 +2,30 @@ use std::io;
 use rand:: Rng;
 
 fn main(){
-    println!("Enter your guess:");
 
     let truth = rand::thread_rng().gen_range(1..=100);
     println!("{truth}");
 
-    let mut guess = String :: new();
-    io:: stdin().read_line(&mut guess).expect("Failed to read guess");
-    
-    let guess : i32 = guess.trim().parse().expect("Please type a number!");
+    loop {
+        println!("Enter your guess:");
 
-    println!("You guessed {}", guess);
+        let mut guess = String :: new();
+        io:: stdin().read_line(&mut guess).expect("Failed to read guess");
+        
+        let guess : i32 = guess.trim().parse().expect("Please type a number!");
 
-    if guess == truth {
-        println!("You guessed correct");
+        println!("You guessed {}", guess);
 
+        if guess == truth {
+            println!("You guessed correct");
+            break;
+
+        }
+        else{
+            println!("Oho Maa chuda");
+            println!("Abe kaha ja raha hai. Chal ek aur baar try kar le.");
+        }
     }
-    else{
-        println!("Oho Maa chuda");
-    }
+    println!("You win!");
 
 }
